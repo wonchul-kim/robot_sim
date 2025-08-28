b383036d37a5859665ac72ad627653cc86f637a2
wonchul <kim.wonchul@aiv.ai>
2025-08-28 13:33:40 +0900

    added open-drawer

--- robot_sim/isaaclab/assets/robots/doosan/lift/joint_pos_env_cfg.py
+++ robot_sim/isaaclab/assets/robots/doosan/lift/joint_pos_env_cfg.py
@@ -13,49 +13,8 @@ from isaaclab_tasks.manager_based.manipulation.lift.lift_env_cfg import LiftEnvC
 # Pre-defined configs
 ##
 from isaaclab.markers.config import FRAME_MARKER_CFG  # isort: skip
-from isaaclab_assets.robots.franka import FRANKA_PANDA_CFG  # isort: skip
 from robot_sim.isaaclab.assets.robots.doosan.m1013_2f140_cfg import M1013_2F140_CFG
 
-'''
-== Link names == 
-/World/ground False
-/World/ground/GroundPlane False
-/World/ground/Environment False
-/World/Origin False
-/World/Origin/Robot False
-/World/Origin/Robot/base True
-/World/Origin/Robot/base/base_link False
-/World/Origin/Robot/base/visuals False
-/World/Origin/Robot/base/collisions False
-/World/Origin/Robot/link_1 True
-/World/Origin/Robot/link_1/visuals False
-/World/Origin/Robot/link_1/collisions False
-/World/Origin/Robot/link_2 True
-/World/Origin/Robot/link_2/visuals False
-/World/Origin/Robot/link_2/collisions False
-/World/Origin/Robot/link_3 True
-/World/Origin/Robot/link_3/visuals False
-/World/Origin/Robot/link_3/collisions False
-/World/Origin/Robot/link_4 True
-/World/Origin/Robot/link_4/visuals False
-/World/Origin/Robot/link_4/collisions False
-/World/Origin/Robot/link_5 True
-/World/Origin/Robot/link_5/visuals False
-/World/Origin/Robot/link_5/collisions False
-/World/Origin/Robot/link_6 True
-/World/Origin/Robot/link_6/tool0 False
-/World/Origin/Robot/link_6/tool0/robotiq_hande_coupler False
-/World/Origin/Robot/link_6/tool0/robotiq_hande_coupler/robotiq_hande_link False
-/World/Origin/Robot/link_6/tool0/robotiq_hande_coupler/robotiq_hande_link/robotiq_hande_end False
-/World/Origin/Robot/link_6/visuals False
-/World/Origin/Robot/link_6/collisions False
-/World/Origin/Robot/robotiq_hande_left_finger True
-/World/Origin/Robot/robotiq_hande_left_finger/visuals False
-/World/Origin/Robot/robotiq_hande_left_finger/collisions False
-/World/Origin/Robot/robotiq_hande_right_finger True
-/World/Origin/Robot/robotiq_hande_right_finger/visuals False
-/World/Origin/Robot/robotiq_hande_right_finger/collisions False
-'''
 
 @configclass
 class M10132F140CubeLiftEnvCfg(LiftEnvCfg):
